5dae7e12343f209a2c6f0fb22693ab5f7c2d87fa
Maria Eduarda <meac21kj@gmail.com>
2025-04-30 16:15:50 -0300

    initial commit

---
+++ src/test/java/com/meac/qrcode_generator/QrcodeGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meac.qrcode_generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrcodeGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
