c2b63f668c7dfb00ae47b80e88ec4a81d4d28271
Maria Eduarda <meac21kj@gmail.com>
2025-05-01 00:23:18 -0300

    feat: QRCodeController

--- src/main/java/com/meac/qrcode_generator/controller/QRCodeController.java
+++ src/main/java/com/meac/qrcode_generator/controller/QRCodeController.java
@@ -1,6 +1,8 @@
 package com.meac.qrcode_generator.controller;
 
 import com.meac.qrcode_generator.dto.QRCodeGenerateRequest;
+import com.meac.qrcode_generator.dto.QRCodeGenerateResponse;
+import com.meac.qrcode_generator.services.QRCodeGeneratorService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,10 +13,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/qrcode")
 public class QRCodeController {
 
-    @PostMapping
-    public ResponseEntity<String> generateQrCode(@RequestBody QRCodeGenerateRequest request) {
+    private final QRCodeGeneratorService qrCodeGeneratorService;
 
-        return null;
+    public QRCodeController(QRCodeGeneratorService qrCodeGeneratorService) {
+        this.qrCodeGeneratorService = qrCodeGeneratorService;
+    }
+
+
+    @PostMapping
+    public ResponseEntity<QRCodeGenerateResponse> generateQrCode(@RequestBody QRCodeGenerateRequest request) {
+
+        try {
+            QRCodeGenerateResponse response = this.qrCodeGeneratorService.generateQRCode(request);
+           return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
+
+
+
 
     }
 }
